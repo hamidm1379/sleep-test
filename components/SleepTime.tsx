64ada1fd992975da3cb1446b3dcb54d7e5d22c40
hamidm1379 <hamidbit99@gmail.com>
2025-09-05 21:38:50 +0330

    change

--- components/SleepTime.tsx
+++ components/SleepTime.tsx
@@ -1,41 +1,11 @@
-'use client';
-
-import { useRef } from 'react';
-import { useSpring, animated } from '@react-spring/web';
-import { useDrag } from '@use-gesture/react';
-
-import Link from 'next/link';
-
 import SetTimer from '@/components/sleeptime/SetTimer';
 import AddSleepTime from '@/components/sleeptime/AddSleepTime';
 import EditSleepTime from '@/components/sleeptime/EditSleepTime';
+import GeneralReportModal from '@/components/sleeptime/GeneralReportModal';
 
 export default function SleepTimeSection() {
-
-    const sheetRef = useRef(null);
-    const windowHeight = typeof window !== 'undefined' ? window.innerHeight : 800;
-
-    const minHeight = windowHeight * 0.001;
-    const maxHeight = windowHeight * 0.3;
-
-    const [{ y }, api] = useSpring(() => ({ y: windowHeight - minHeight }));
-    const handleOverlayClick = () => {
-        api.start({ y: windowHeight - minHeight });
-    };
-
-    const bind = useDrag(
-        ({ movement: [, my], memo = y.get() }) => {
-            let newY = memo + my;
-            if (newY < windowHeight - maxHeight) newY = windowHeight - maxHeight;
-            if (newY > windowHeight - minHeight) newY = windowHeight - minHeight;
-            api.start({ y: newY });
-            return memo;
-        },
-        { from: () => [0, y.get()], filterTaps: true }
-    );
-
     return (
-        <section onClick={handleOverlayClick} className="sm:px-[20px] md:px-[200px] lg:px-[400px] pt-3">
+        <section className="sm:px-[20px] md:px-[200px] lg:px-[400px] pt-3">
 
             <SetTimer />
 
@@ -43,40 +13,7 @@ export default function SleepTimeSection() {
 
             <AddSleepTime />
 
-            <animated.div
-                {...bind()}
-                ref={sheetRef}
-                className="fixed left-0 right-0 bottom-0 bg-white max-[400px]:w-full w-[400px]  shadow-xl touch-pan-y z-10 mx-auto"
-                style={{
-                    y,
-                    height: windowHeight,
-                    willChange: 'transform',
-                }}
-            >
-                <div className="p-4 overflow-auto h-full bg-[#9642FF] rounded-t-3xl">
-                    <div className="w-16 h-1.5 bg-gray-300 rounded-full mx-auto mt-2"></div>
-                    <h2 className="text-lg font-bold mb-2 text-center max-[400px]:text-[20px] text-[24px] mt-3 text-white">گزارش کلی</h2>
-                    <div className="mt-10">
-                        <div className="grid grid-cols-2 gap-6 mb-8 text-white">
-                            <div className="text-center">
-                                <p className="text-sm opacity-90">تاریخ</p>
-                                <p className="text-2xl font-semibold mt-1">۱۴۰۳/۰۱/۱۳</p>
-                            </div>
-                            <div className="text-center">
-                                <p className="text-sm opacity-90">مدت زمان</p>
-                                <p className="text-2xl font-semibold mt-1">۰۷:۵۳</p>
-                            </div>
-                        </div>
-                        <Link href="/generalreport">
-                            <div className="flex justify-center rounded-[8px] px-[24px] mt-4 mx-auto gap-2 max-[400px]:w-[280px] w-[320px] h-[48px] bg-white border-1 border-[#A85FFF] cursor-pointer hover:bg-[#e4dfe9]">
-                                <button className='cursor-pointer'>
-                                    مشاهده کلی
-                                </button>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-            </animated.div>
+            <GeneralReportModal />
 
         </section>
     );
--- components/sleeptime/GeneralReportModal.tsx
+++ components/sleeptime/GeneralReportModal.tsx
@@ -1,5 +1,65 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
+
 export default function GeneralReportModal() {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const bottomSectionRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isExpanded && bottomSectionRef.current && !bottomSectionRef.current.contains(event.target)) {
+                setIsExpanded(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isExpanded]);
+
     return (
-        <></>
+        <div
+            ref={bottomSectionRef}
+            className={`${isExpanded ? 'h-64' : 'h-20'
+                } fixed left-0 right-0 bottom-0 max-[400px]:w-full w-[400px] bg-[#9642FF] mx-auto rounded-t-3xl transition-all duration-300 ease-in-out cursor-pointer  shadow-lg z-10`}
+            onClick={() => setIsExpanded(!isExpanded)}
+        >
+            <div className="">
+                {isExpanded ? (
+                    <>
+                        <div className="w-16 h-1.5 bg-gray-300 rounded-full mx-auto mt-2"></div>
+                        <h2 className="text-lg font-medium mb-2 text-center max-[400px]:text-[20px] text-[24px] mt-3 text-white">گزارش کلی</h2>
+                        <div className="mt-10">
+                            <div className="grid grid-cols-2 gap-6 mb-8 text-white">
+                                <div className="text-center">
+                                    <p className="text-sm opacity-90">تاریخ</p>
+                                    <p className="text-2xl font-medium mt-1">۱۴۰۳/۰۱/۱۳</p>
+                                </div>
+                                <div className="text-center">
+                                    <p className="text-sm opacity-90">مدت زمان</p>
+                                    <p className="text-2xl font-medium mt-1">۰۷:۵۳</p>
+                                </div>
+                            </div>
+                            <Link href="/generalreport">
+                                <div className="flex justify-center rounded-[8px] px-[24px] mt-4 mx-auto gap-2 max-[400px]:w-[280px] w-[320px] h-[48px] bg-white border-1 border-[#A85FFF] cursor-pointer hover:bg-[#e4dfe9]">
+                                    <button className='cursor-pointer'>
+                                        مشاهده کلی
+                                    </button>
+                                </div>
+                            </Link>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="w-16 h-1.5 bg-gray-300 rounded-full mx-auto mt-2"></div>
+                        <h2 className="text-lg font-medium mb-2 text-center max-[400px]:text-[20px] text-[24px] mt-3 text-white">گزارش کلی</h2>
+                    </>
+                )}
+            </div>
+        </div>
     );
 }
